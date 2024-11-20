55f3dcbbd0d9380d0acebb2712d5b6e7f83e4759
Yara Hassan <160149907+yara123456789yara@users.noreply.github.com>
2024-11-20 21:50:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector(".phone");
+let container= document.querySelector(".container");
+//console.log(phone);
+
+function change(phone) {
+    img.src = phone;
+
+}
+function colors(color) {
+    container.style.backgroundColor = color;
+}
